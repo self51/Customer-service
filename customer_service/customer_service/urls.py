88050d8952015ead6dddf7655562b72ed26b7ee3
self <ggdiscone@gmail.com>
2023-01-24 21:17:25 +0200

    Home page created

--- customer_service/customer_service/urls.py
+++ customer_service/customer_service/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from django.views.generic.base import TemplateView
+
+
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('authentication.urls')),
