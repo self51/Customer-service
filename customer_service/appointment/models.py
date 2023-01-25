c0e2d4b3fe5f8332935a2306f7d4ff9a0c06da10
self <ggdiscone@gmail.com>
2023-01-25 15:51:12 +0200

    Added new rows in db such as customer and weekday

--- customer_service/appointment/models.py
+++ customer_service/appointment/models.py
@@ -3,10 +3,22 @@ from django.db import models
 from authentication.models import User
 from worker.models import Location, Schedule
 
+WEEKDAYS_CHOICES = (
+    (1, 'Monday'),
+    (2, 'Tuesday'),
+    (3, 'Wednesday'),
+    (4, 'Thursday'),
+    (5, 'Friday'),
+    (6, 'Saturday'),
+    (7, 'Sunday'),
+)
+
 
 class Appointment(models.Model):
-    worker = models.ForeignKey(User, on_delete=models.CASCADE)
-    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='customers')
+    worker = models.ForeignKey(User, on_delete=models.CASCADE, related_name='workers')
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='locations')
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     time = models.TimeField()
+    weekday = models.IntegerField(choices=WEEKDAYS_CHOICES)
     status = models.BooleanField(default=False)
